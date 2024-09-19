b8478a7bcc7a7c9cc492a01427b80e7c4d5f3abd
owostline <ostlinecasao@gmail.com>
2024-09-19 14:26:24 +0800

    Code clean up + Logo and title

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,33 +1,37 @@
-import { features } from "../constants"
+import { features } from "../constants";
 const FeatureSection = () => {
   return (
-    <div className = "relative mt-20 border-b border-neutral-800 min-h-[600px] ">
-        <div className = "text-center">
-            <span className = "bg-neutral-800 text-purple-400 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase"> 
-            Features
-            </span>
-            <h2 className = "text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-15 tracking-wide">
-            <span className = "bg-gradient-to-r from-violet-400 to-violet-700 text-transparent bg-clip-text">Purr-fection</span> in Every Keystroke
-            </h2>
-        </div>
-        <div className="flex flex-wrap mt-10 lg:mt-20">
-            {features.map((feature, index) => (
-                <div key = {index} className="w-full sm:w-1/2 lg:w-1/3">
-                    <div className="flex mx-7">
-                        <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-pink-400 justify-center items-center rounded-full">
-                            {feature.icon}
-                        </div>
-                        <div className="flex flex-col">
-                            <h5 className="mt-1 mb-2 lg:mb-4 text-xl">{feature.text}</h5>
-                            <p className="mb-11 text-neutral-300 text-xs lg:text-base">{feature.description}</p>
-                        </div>
-                    </div>
-                </div>
-            ))}
-            
-        </div>
+    <div className="relative mt-20 border-b border-neutral-800 min-h-[600px] ">
+      <div className="text-center">
+        <span className="bg-neutral-800 text-purple-400 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
+          Features
+        </span>
+        <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-15 tracking-wide">
+          <span className="bg-gradient-to-r from-violet-400 to-violet-700 text-transparent bg-clip-text">
+            Purr-fection
+          </span>{" "}
+          in Every Keystroke
+        </h2>
+      </div>
+      <div className="flex flex-wrap mt-10 lg:mt-20">
+        {features.map((feature, index) => (
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
+            <div className="flex mx-7">
+              <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-pink-400 justify-center items-center rounded-full">
+                {feature.icon}
+              </div>
+              <div className="flex flex-col">
+                <h5 className="mt-1 mb-2 lg:mb-4 text-xl">{feature.text}</h5>
+                <p className="mb-11 text-neutral-300 text-xs lg:text-base">
+                  {feature.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeatureSection
+export default FeatureSection;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,35 +5,50 @@ import { checklistItems } from "../constants";
 const Products = () => {
   return (
     <div className="mt-16">
-        <div className = "text-center">
-            <span className = "bg-neutral-800 text-purple-400 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase"> 
-            Products
-            </span>
+      <div className="text-center">
+        <span className="bg-neutral-800 text-purple-400 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
+          Products
+        </span>
+      </div>
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
+        Unleash Your
+        <span className="bg-gradient-to-r from-violet-400 to-violet-700 text-transparent bg-clip-text">
+          {" "}
+          Keyboard’s{" "}
+        </span>
+        Personality.
+      </h2>
+      <div className="flex flex-wrap justify-center items-center">
+        <div className="pt-5 lg:p-2 w-2/3 md:w-5/12 lg:w-4/12">
+          <img src={productImg}></img>
         </div>
-        <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">Unleash Your 
-            <span className="bg-gradient-to-r from-violet-400 to-violet-700 text-transparent bg-clip-text"> Keyboard’s </span>
-        Personality.</h2>
-        <div className="flex flex-wrap justify-center items-center">
-            <div className="pt-5 lg:p-2 w-2/3 md:w-5/12 lg:w-4/12">
-                <img src={productImg}></img>
-            </div>
-            <div className="flex-col pt-5 lg:pt-12 w-full lg:w-1/2">
-                {checklistItems.map((item, index) => (
-                    <div key={index} className="flex mb-12 items-start md:mx-24 lg:mx-0">
-                        <div className="text-pink-400 mx-6 bg-neutral-900 w-10 p-2 justify-center items-center rounded-full">
-                            <Heart />
-                        </div>
-                        
-                        <div>
-                            <a href="#" className="mt-1 mb-2 text-xl font-semibold underline hover:text-pink-300">{item.title}</a>
-                            <p className="text-xs lg:text-base mr-7 text-neutral-300">{item.description}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    </div>
-  )
-}
+        <div className="flex-col pt-5 lg:pt-12 w-full lg:w-1/2">
+          {checklistItems.map((item, index) => (
+            <div
+              key={index}
+              className="flex mb-12 items-start md:mx-24 lg:mx-0"
+            >
+              <div className="text-pink-400 mx-6 bg-neutral-900 w-10 p-2 justify-center items-center rounded-full">
+                <Heart />
+              </div>
 
-export default Products
+              <div>
+                <a
+                  href="#"
+                  className="mt-1 mb-2 text-xl font-semibold underline hover:text-pink-300"
+                >
+                  {item.title}
+                </a>
+                <p className="text-xs lg:text-base mr-7 text-neutral-300">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
